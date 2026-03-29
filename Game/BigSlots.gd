53ea93a03967b2c42b001b77001776b81f35d9a9
Anthony <189166184+Anthony-Queen@users.noreply.github.com>
2026-03-29 14:29:41 +0200

    Literally cant figure out how to cycle the symbols sprites

--- Game/BigSlots.gd
+++ Game/BigSlots.gd
@@ -1,6 +1,10 @@
 extends PanelContainer
-@onready var wasButtonPressed: bool = false
 
 func _ready():
+	if $Sprite and $Sprite2:
+		spin()
+
+func spin():
 	var tw = get_tree().create_tween()
 	tw.tween_property($Sprite, "position", Vector2(85, 600), 1)
+	tw.parallel().tween_property($Sprite2, "position", Vector2(85, 600), 1)
