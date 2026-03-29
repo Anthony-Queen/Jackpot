b329ae32b8e079dc26bacbeadc6b9ce23ea1cc90
Anthony-Queen <189166184+Anthony-Queen@users.noreply.github.com>
2026-03-29 21:48:46 +0200

    Get the sprites to loop, FINALLY

--- Game/BigSlots.gd
+++ Game/BigSlots.gd
@@ -1,7 +1,10 @@
 extends PanelContainer
 
+#set_process(false) # Stops _process
+var spins: int = 0
 var speed := 2000
-var sprite_height := 4460
+var sprite_height := 1142
+var positions: Array = [0, ]
 
 func _process(delta):
 	$Sprite.position.y += speed * delta
@@ -9,8 +12,9 @@ func _process(delta):
 	
 	if $Sprite.position.y >= sprite_height:
 		$Sprite.position.y = $Sprite2.position.y - sprite_height
-		print("Worky 1")
 	
 	if $Sprite2.position.y >= sprite_height:
 		$Sprite2.position.y = $Sprite.position.y - sprite_height
-		print("Worky 2")
+		spins += 1
+	if spins == 3:
+		set_process(false)
