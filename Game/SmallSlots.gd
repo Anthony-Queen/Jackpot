13e1a2a7867333ff2667f631e7b07e774c576b5b
Anthony-Queen <189166184+Anthony-Queen@users.noreply.github.com>
2026-03-30 20:50:34 +0200

    Try implementing clean stopping in sprites, still not working

--- Game/SmallSlots.gd
+++ Game/SmallSlots.gd
@@ -2,11 +2,14 @@ extends PanelContainer
 
 const speed : int = 500
 const sprite_height : int = 628
+var chosenSymbol
+var currentSprite
 signal pressed
-# All Symbol Probabilities!!!!:
+
+# All Symbol Stuff:
 const SYMBOLS = {
 	"seven":  Rect2(0.0, 3837.5, 552.0, 622.5),
-	"prugna": Rect2(0.0, 3233.8, 552.0, 589.3),
+	"plum": Rect2(0.0, 3233.8, 552.0, 589.3),
 	"bar":    Rect2(0.0, 2537.0, 552.0, 668.7),
 	"melon":  Rect2(0.0, 2017.7, 552.0, 408.3),
 	"bell":   Rect2(0.0, 1465.0, 552.0, 471.1),
@@ -15,9 +18,20 @@ const SYMBOLS = {
 	"lemon":  Rect2(0.0, 0.0, 552.0, 418.0)
 }
 
-
+# More entries = Higher chance of getting picked
+var reel = [
+	"cherry","cherry","cherry","cherry","cherry", "cherry", # 24%
+	"lemon","lemon","lemon","lemon", "lemon", # 20%
+	"orange","orange","orange", "orange", # 16%
+	"plum", "plum", "plum", # 12%
+	"melon","melon", "melon", # 12%
+	"bell", "bell", # 8%
+	"bar", # 4%
+	"seven", # 4%
+]
 
 func _ready():
+	chosenSymbol = reel.pick_random()
 	pressed.connect(_on_pressed)
 
 func _process(delta):
@@ -26,13 +40,18 @@ func _process(delta):
 	
 	if $Sprite.position.y >= sprite_height:
 		$Sprite.position.y = $Sprite2.position.y - sprite_height
-	
+		currentSprite = $Sprite
 	if $Sprite2.position.y >= sprite_height:
 		$Sprite2.position.y = $Sprite.position.y - sprite_height
+		currentSprite = $Sprite2
 
 func _input(event):
 	if event is InputEventKey and event.pressed and event.keycode in [KEY_ENTER, KEY_SPACE]:
 		pressed.emit()
 
 func _on_pressed():
+	if currentSprite == $Sprite:
+		$Sprite.position.y = SYMBOLS[chosenSymbol].position.y
+	else:
+		$Sprite2.position.y = SYMBOLS[chosenSymbol].position.y
 	set_process(false)
